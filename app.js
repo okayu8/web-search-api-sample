606f673818ece61d6e508641a8e833916f9d752b
片岡祐太郎 <hitugaya12@gmail.com>
2018-11-24 04:26:18 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+
+var btn = document.getElementById('btn');
+var content = document.getElementById('content');
+
+
+
+
+//音声認識APIの使用
+var speech = new webkitSpeechRecognition();
+
+
+//言語を日本語に設定
+speech.lang = "ja";
+
+
+
+
+btn.addEventListener( 'click' , function() {
+
+
+    // 音声認識をスタート
+    speech.start();
+
+
+
+
+} );
+
+
+
+
+speech.addEventListener( 'result' , function( e ) {
+
+
+    var text = e.results[0][0].transcript;
+
+
+    // 認識された「言葉(text)」を、表示用のdivタグに代入する
+    content.textContent = text;
+
+} );
